d1669ec6f1dd6c87ecca0f80940f92bea1903eea
DominMFD <mfdmatheus@hotmail.com>
2024-02-07 11:14:44 -0300

    feat: food page implementation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,39 @@
-import {View, Text, FlatList} from 'react-native'
-import { CATEGORIES } from "@/utils/data/products"
+import {View, Text, FlatList, SectionList} from 'react-native'
+import { CATEGORIES, MENU } from "@/utils/data/products"
 import { Header } from '@/components/header'
 import { CategoryButton } from '@/components/category-button'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import { Product } from '@/components/product'
+import { Link } from "expo-router"
+import { useCartStore } from '@/stores/cart-store'
 
 export default function Home() {
 
+    const cartStore = useCartStore()
+
     const [category, setCategory] = useState(CATEGORIES[0])
+
+    const sectionListRef = useRef<SectionList>(null)
+
+    const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0) 
 
     function handleCategories(item: string) {
         setCategory(item)
+
+        const sectionIndex = CATEGORIES.findIndex((category) => category === item)
+
+        if(sectionListRef.current) {
+            sectionListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0
+            })
+        }
     }   
 
     return (
         <View className="flex-1 pt-8">
-            <Header title="Cardápio"/>
+            <Header title="Cardápio" cartQuantityItems={cartQuantityItems}/>
 
             <FlatList 
                 data={CATEGORIES}
@@ -26,6 +45,24 @@ export default function Home() {
                 contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
                 showsHorizontalScrollIndicator={false}
             />
+
+            <SectionList
+             ref={sectionListRef}
+             sections={MENU}
+             keyExtractor={(item) => item.id}
+             stickySectionHeadersEnabled={false}
+             renderItem={({item}) => (
+                <Link href={`/product/${item.id}`} asChild>
+                    <Product data={item}/>
+                </Link>
+             )}
+             renderSectionHeader={({section: {title}}) => (
+             <Text className='text-xl text-white font-heading mt-8 mb-3'>{title}</Text>)}
+
+             className='flex-1 p-5'
+             showsVerticalScrollIndicator={false}
+             contentContainerStyle={{paddingBottom: 100}}
+             />
             
         </View>
     )
